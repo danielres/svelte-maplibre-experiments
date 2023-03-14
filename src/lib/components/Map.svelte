dc61e1b6b6da71008213cba129ceae4a5daf52b5
Daniel Reszka <dannn.r@gmail.com>
2023-03-14 12:21:18 +0100

    Merge branch 'component-based-controls' into 01-everything

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import Background from './layers/Background.svelte';
+	import NavigationControl from './NavigationControl.svelte';
 	import superMap from './superMap';
 
 	const { useMap, addMarker, removeMarker, markers, map } = superMap();
 
 	let withBackground = false;
+	let withNavigationControl = true;
 </script>
 
 <div class="min-h-screen" use:useMap={{ center: [5, 50], zoom: 4 }}>
@@ -13,11 +15,24 @@
 		{#if withBackground}
 			<Background map={$map} color="#FFF" opacity={0.5} />
 		{/if}
+		{#if withNavigationControl}
+			<NavigationControl {map} />
+		{/if}
 	{/if}
 </div>
 
-<div class="absolute w-[200px] top-8 left-6">
+<div class="absolute w-[200px] top-8 left-6 text-white">
 	<div class="bg-sky-600 p-4 rounded-xl grid gap-4">
+		<label class="flex items-center justify-between">
+			<span>Navigation control</span>
+
+			<input
+				type="checkbox"
+				bind:checked={withNavigationControl}
+				class="appearance-none border-2 border-sky-500 rounded w-4 h-4 checked:bg-sky-300  focus:outline-none"
+			/>
+		</label>
+
 		<button
 			class="bg-sky-500 p-2 rounded-lg text-white w-full"
 			on:click={() => (withBackground = !withBackground)}
@@ -26,13 +41,13 @@
 			Toggle background
 		</button>
 
-		<button class="bg-sky-500 p-2 rounded-lg text-white w-full" on:click={addMarker} type="button">
+		<button class="bg-sky-500 p-2 rounded-lg w-full" on:click={addMarker} type="button">
 			Add marker
 		</button>
 
 		{#if $markers.length}
 			<div class="grid gap-2">
-				<h3 class="text-lg text-white">Markers</h3>
+				<h3 class="text-lg">Markers</h3>
 
 				{#each $markers as marker}
 					<button
---
+++ src/lib/components/NavigationControl.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Writable } from 'svelte/store';
+
+	import maplibregl from 'maplibre-gl';
+	import { onMount, onDestroy } from 'svelte';
+
+	export let map: Writable<maplibregl.Map | undefined>;
+
+	let control: maplibregl.NavigationControl;
+
+	onMount(() => {
+		if (!$map) return;
+		control = new maplibregl.NavigationControl({});
+		$map.addControl(control, 'top-right');
+	});
+
+	onDestroy(() => {
+		if (!$map) return;
+		$map.removeControl(control);
+	});
+</script>
